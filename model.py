0e16a979a45343ed3f015d8b0cabe6deffc08158
Alessandro <avirmond2000@gmail.com>
2021-07-22 20:06:15 -0300

    poliformismo implementado

--- model.py
+++ model.py
@@ -2,22 +2,22 @@ class Programa:
     def __init__(self, nome, ano):
         self._nome = nome.title()
         self.ano = ano
-        self._like = 0
+        self._likes = 0
 
-        @property
-        def likes(self):
-            return self._likes
+    @property
+    def likes(self):
+        return self._likes
 
-        def dar_like(self):
-            self._likes += 1
+    def dar_likes(self):
+        self._likes += 1
 
-        @property
-        def nome(self):
-            return self._nome
+    @property
+    def nome(self):
+        return self._nome
 
-        @nome.setter
-        def nome(self, novo_nome):
-            self._nome = novo_nome.title()
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
@@ -30,13 +30,19 @@ class Serie(Programa):
         self.temporadas = temporadas
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-f'- Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
-
-
 atlanta = Serie('atlanta', 2018, 2)
-atlanta.dar_like()
-atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-f'- Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+print(f'Nome: {vingadores.nome} - Likes: {vingadores.likes}')
+print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D - {programa.likes}')
