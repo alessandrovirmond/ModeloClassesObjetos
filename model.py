abc25525733605d44ec6e27328dd79361404003a
Alessandro <avirmond2000@gmail.com>
2021-08-01 21:57:53 -0300

    atualizando representacao textual de objetos

--- model.py
+++ model.py
@@ -24,16 +24,16 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} Likes')
+    def __str__(self):
+        return f'{self._nome} - {self.temporadas} - {self._likes} Likes'
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 atlanta = Serie('atlanta', 2018, 2)
@@ -50,4 +50,4 @@ print(f'Nome: {atlanta.nome} - Likes: {atlanta.likes}')
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(Programa)
